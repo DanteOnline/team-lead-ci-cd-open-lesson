e1e6494b026105bd5ca662f8bc5f29e4ad54a18d
DanteOnline <iamdanteonline@gmail.com>
2024-09-09 20:56:40 +0300

    fix linter

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+"""
+Main module
+"""
 from say_something import show_text
 
 
--- testing/test_say_something/test_init.py
+++ testing/test_say_something/test_init.py
@@ -1,6 +1,13 @@
+"""
+Test Init
+"""
 from say_something import show_text
 
 
 def test_show_text():
+    """
+    Test show text
+    :return:
+    """
     show_text('test text')
     assert True
